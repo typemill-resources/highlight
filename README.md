87fa28c345c1f9771972b26a3ceb542673e66131
succeun <succeun@gmail.com>
2021-05-01 22:25:27 +0900

    added Selecting highlight.js theme

---
+++ README.md
@@ -0,0 +1,13 @@
+# highlight
+Highlight your code examples with highlight.js for Typemill
+
+## Adding feature
+- Selecting highlight.js theme
+
+## Pretreatment
+- Remove the code below in `/theme/cyanine/css/style.css`
+```css
+/* Fix background for hightlight plugin */
+code.hljs{ background: transparent; }
+```
+
--- highlight.php
+++ highlight.php
@@ -11,15 +11,29 @@ class Highlight extends Plugin
     public static function getSubscribedEvents()
     {
 		return array(
+			'onSettingsLoaded'		=> 'onSettingsLoaded',		
 			'onTwigLoaded' 			=> 'onTwigLoaded'
 		);
     }
 	
+	public function onSettingsLoaded($settings)
+	{
+		$this->settings = $settings->getData();
+	}
+	
 	
 	public function onTwigLoaded()
 	{
+		$highlightSettings = $this->settings['settings']['plugins']['highlight'];
+		
 		/* add external CSS and JavaScript */
-		$this->addCSS('/highlight/public/default.css');
+		$this->addCSS('/highlight/public/reset.css');	// For theme code css reset
+		if (isset($highlightSettings['theme'])) {
+			$this->addCSS('/highlight/public/'.$highlightSettings['theme'].'.css');
+		} else {
+			$this->addCSS('/highlight/public/default.css');
+		}
+		
 		$this->addJS('/highlight/public/highlight.pack.js');
 	
 		/* initialize the script */
