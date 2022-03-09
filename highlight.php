083cd244551c9efc2ee558ae141f543291e9084a
MysterieDev <josefsle97@gmail.com>
2022-03-09 13:35:11 +0100

    update

--- highlight.php
+++ highlight.php
@@ -36,9 +36,14 @@ class Highlight extends Plugin
 		
 		$this->addJS('/highlight/public/highlight.pack.js');
 		$this->addJS('/highlight/public/highlightjs-line-numbers.min.js');
-	
+
+		/* initialize copy badge */
+		$this->addCss('/highlight/public/highlightjs-copy.min.css');
+		$this->addJS('/highlight/public/highlightjs-copy.min.js');
+		$this->addInlineJs('hljs.addPlugin(new CopyButtonPlugin());');
 		/* initialize the script */
-		$this->addInlineJS('hljs.initHighlightingOnLoad();');
+		$this->addInlineJS('hljs.highlightAll();');
+		
 		
 		if (isset($highlightSettings['lineNumber']) && $highlightSettings['lineNumber'] == 'true') {
 			$this->addInlineJS('hljs.initLineNumbersOnLoad({ singleLine: true });');
