ae8790a40118a344b4d86b22b27f6591b8dc478a
trendschau <trendschau@gmail.com>
2025-06-09 08:28:04 +0200

    V 2.2.0 fix libraries and add word wrap option

--- highlight.php
+++ highlight.php
@@ -21,26 +21,26 @@ class Highlight extends Plugin
 			$highlightSettings = $this->getPluginSettings();
 			
 			/* add external CSS and JavaScript */
-			$this->addCSS('/highlight/public/reset.css');	// For theme code css reset
+			$this->addCSS('/highlight/themes/reset.css');	// For theme code css reset
 			if (isset($highlightSettings['theme']))
 			{
-				$this->addCSS('/highlight/public/'.$highlightSettings['theme'].'.css');
+				$this->addCSS('/highlight/themes/'.$highlightSettings['theme'].'.css');
 			}
 			else
 			{
-				$this->addCSS('/highlight/public/default.css');
+				$this->addCSS('/highlight/themes/default.css');
 			}
 			
-			$this->addJS('/highlight/public/highlight.pack.js');
-			$this->addJS('/highlight/public/highlightjs-line-numbers.min.js');
+			$this->addJS('/highlight/js/highlight.min.js');
+			$this->addJS('/highlight/js/highlightjs-line-numbers.min.js');
 
 			if (isset($highlightSettings['copyButton']) && $highlightSettings['copyButton'] == 'true')
 			{
 				/* initialize copy badge 
 				https://github.com/arronhunt/highlightjs-copy
 				*/
-				$this->addCss('/highlight/public/highlightjs-copy.min.css');
-				$this->addJS('/highlight/public/highlightjs-copy.min.js');
+				$this->addCss('/highlight/js/highlightjs-copy.min.css');
+				$this->addJS('/highlight/js/highlightjs-copy.min.js');
 				$this->addInlineJs('hljs.addPlugin(new CopyButtonPlugin());');
 			}
 			/* initialize the script */
@@ -49,6 +49,15 @@ class Highlight extends Plugin
 			if (isset($highlightSettings['lineNumber']) && $highlightSettings['lineNumber'] == 'true')
 			{
 				$this->addInlineJS('hljs.initLineNumbersOnLoad({ singleLine: true });');
+			}
+
+			if (isset($highlightSettings['wordWrap']) && $highlightSettings['wordWrap'] == 'true')
+			{
+				$this->addInlineCSS('
+					code.hljs {
+						  white-space: pre-wrap;
+						}
+					');
 			}
 		}
 	}
